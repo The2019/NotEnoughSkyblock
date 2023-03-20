a3182d01f1b3cd196d157591cd2330d8b2e87648
theokoppler <theo.koppler@icloud.com>
2023-03-20 15:43:31 +0100

    Button Working

--- src/main/java/net/the2019/skyblock/mixin/chatRenderMixin.java
+++ src/main/java/net/the2019/skyblock/mixin/chatRenderMixin.java
@@ -1,15 +1,32 @@
 package net.the2019.skyblock.mixin;
 
-import net.minecraft.client.gui.screen.ChatScreen;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.ingame.InventoryScreen;
+import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.text.LiteralText;
+import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(ChatScreen.class)
-public class chatRenderMixin {
+@Mixin(InventoryScreen.class)
+public abstract class chatRenderMixin extends Screen {
+    @Shadow private boolean mouseDown;
+    private static final MinecraftClient mc = MinecraftClient.getInstance();
 
-    @Inject(method = "init", at = @At("HEAD"),cancellable = true)
-    private void renderChat( CallbackInfo ci){
+    protected chatRenderMixin(Text title) {
+        super(title);
+    }
+
+    @Inject(method = "render", at = @At("HEAD"))
+    private void renderChat( CallbackInfo ci) {
+        assert this.client != null;
+        assert this.client.player != null;
+        this.addDrawableChild(new ButtonWidget(10 , 10, 90, 20, new LiteralText("Config") , (buttonWidget) -> {
+            this.client.player.sendChatMessage("/warp hub");
+        }));
     }
 }
