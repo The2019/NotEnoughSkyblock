cb8d3ce96eee746a11783bbbaa356216fb5c5ca7
theokoppler <theo.koppler@icloud.com>
2023-03-20 15:40:48 +0100

    Initial commit

---
+++ src/main/java/net/the2019/skyblock/NotEnoughSkyblock.java
@@ -0,0 +1,13 @@
+package net.the2019.skyblock;
+
+import net.fabricmc.api.ModInitializer;
+import net.hypixel.api.HypixelAPI;
+
+public class NotEnoughSkyblock implements ModInitializer {
+	private static final String API_KEY = "99de5340-7e90-44f2-a974-71ce9ef7901b";
+	public static HypixelAPI API;
+
+	@Override
+	public void onInitialize() {
+	}
+}
