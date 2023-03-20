d371008dc9862f5b550b3d8da2722d9a3aa65a58
theokoppler <theo.koppler@icloud.com>
2023-03-20 21:13:20 +0100

    NotEnoughSkyblock

--- src/main/java/net/the2019/skyblock/NotEnoughSkyblock.java
+++ src/main/java/net/the2019/skyblock/NotEnoughSkyblock.java
@@ -1,13 +1,33 @@
 package net.the2019.skyblock;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.command.v1.ClientCommandManager;
 import net.hypixel.api.HypixelAPI;
+import net.hypixel.api.apache.ApacheHttpClient;
+import net.hypixel.api.example.ExampleUtil;
+
+import java.util.UUID;
 
 public class NotEnoughSkyblock implements ModInitializer {
-	private static final String API_KEY = "99de5340-7e90-44f2-a974-71ce9ef7901b";
 	public static HypixelAPI API;
+	private UUID playerUUID;
 
 	@Override
 	public void onInitialize() {
+
+		String key = System.getProperty("apiKey", "99de5340-7e90-44f2-a974-71ce9ef7901b"); // arbitrary key, replace with your own to test or use the property
+		API = new HypixelAPI(new ApacheHttpClient(UUID.fromString(key)));
+
+
+		// Register the /info command
+		ClientCommandManager.DISPATCHER.register(ClientCommandManager.literal("info").executes(context -> {
+
+			return bazaar(String "skyblock/bazaar/seed");
+		}));
+	}
+
+	public static void bazaar(String args) {
+		ExampleUtil.API.getSkyBlockBazaar().whenComplete(ExampleUtil.getTestConsumer());
+		ExampleUtil.await();
 	}
 }
